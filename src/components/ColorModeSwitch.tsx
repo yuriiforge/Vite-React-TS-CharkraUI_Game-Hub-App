aaec865e9864495c2f5a5b63836d70d48683d8cd
Yurii <stepanuk.ura2001@gmail.com>
2025-07-22 15:15:29 +0300

    Fix color mode text

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,11 +1,15 @@
 import { HStack, Text } from '@chakra-ui/react';
-import { ColorModeButton } from './ui/color-mode';
+import { ColorModeButton, useColorMode } from './ui/color-mode';
 
 const ColorModeSwitch = () => {
+  const { colorMode } = useColorMode();
+
   return (
     <HStack borderRadius="full">
       <ColorModeButton marginStart="auto" padding={4} />
-      <Text whiteSpace="nowrap">Dark Mode</Text>
+      <Text whiteSpace="nowrap">
+        {colorMode === 'dark' ? 'Dark Mode' : 'Light Mode'}
+      </Text>
     </HStack>
   );
 };
