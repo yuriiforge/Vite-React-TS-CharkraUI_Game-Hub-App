74cf8a47f830e18bb0d802659044604a2e28adbc
Yurii <stepanuk.ura2001@gmail.com>
2025-07-24 10:29:39 +0300

    Handle error routes

--- src/router.tsx
+++ src/router.tsx
@@ -3,11 +3,13 @@ import { routes } from './routes.ts';
 import Layout from './pages/Layout.tsx';
 import HomePage from './pages/HomePage.tsx';
 import GameDetailPage from './pages/GameDetailPage.tsx';
+import ErrorPage from './pages/ErrorPage.tsx';
 
 const router = createBrowserRouter([
   {
     path: routes.base,
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: routes.gamesId, element: <GameDetailPage /> },
